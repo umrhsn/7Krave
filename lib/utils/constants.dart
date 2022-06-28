51450b74b08a0a5f3ea5c43cfc8b60b653758001
Umar Hasan <umrhsnE2020@gmail.com>
2022-06-28 13:38:07 +0200

    added SmoothPageIndicator to on_boarding_screen.dart
    finished on_boarding_screen.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const Color kGrey = Color(0xFFFAF2E7);
+  static const Color kButtonLightGrey = Color(0xFFFAF2E7);
+  static const Color kDotLightGrey = Color(0xFFE8E8E8);
+  static const Color kLightOrange = Color(0xFFECBC74);
 }
